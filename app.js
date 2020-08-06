d562590ef9b1354d3c0c47d2187666b3b5f16f20
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-06 00:42:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(__dirname + '/app/public'));
 
 // build mongo database connection url //
 
-process.env.DB_HOST = process.env.DB_HOST || 'localhost'
+process.env.DB_HOST = process.env.DB_HOST || 'https://cloud.casi.io'
 process.env.DB_PORT = process.env.DB_PORT || 27017;
 process.env.DB_NAME = process.env.DB_NAME || 'node-login';
 
