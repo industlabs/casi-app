7716251dfad3f521a217ecb36320c40bafb9a17d
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-06 00:48:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + '/app/public'));
 
 // build mongo database connection url //
 
-process.env.DB_HOST = process.env.DB_HOST || '54.213.100.27'
+process.env.DB_HOST = process.env.DB_HOST || 'localhost'
 process.env.DB_PORT = process.env.DB_PORT || 27017;
 process.env.DB_NAME = process.env.DB_NAME || 'node-login';
 
