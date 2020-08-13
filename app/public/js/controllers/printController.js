35213a1f81e5ca0c4a7c0f7b5e11174dc5683a01
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-13 14:34:03 -0500

    Update printController.js

--- app/public/js/controllers/printController.js
+++ app/public/js/controllers/printController.js
@@ -1,12 +1,32 @@
-
-function PrintController()
+function HomeController()
 {
 // bind event listeners to button clicks //
 	var that = this;
 
 // handle user logout //
 	$('#btn-logout').click(function(){ that.attemptLogout(); });
-	}
+
+// confirm account deletion //
+//**	$('#account-form-btn1').click(function(){$('.modal-confirm').modal('show')});
+
+// handle account deletion //
+//**	$('.modal-confirm .submit').click(function(){ that.deleteAccount(); });
+
+//**	this.deleteAccount = function()
+//**	{
+//**		$('.modal-confirm').modal('hide');
+//**		var that = this;
+//**		$.ajax({
+//**			url: '/delete',
+//**			type: 'POST',
+//**			success: function(data){
+//**	 			that.showLockedAlert('Your account has been deleted.<br>Redirecting you back to the homepage.');
+//**			},
+//**			error: function(jqXHR){
+//**				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
+//**			}
+//**		});
+//**	}
 
 	this.attemptLogout = function()
 	{
@@ -23,5 +43,22 @@ function PrintController()
 			}
 		});
 	}
+
+	this.showLockedAlert = function(msg){
+		$('.modal-alert').modal({ show : false, keyboard : false, backdrop : 'static' });
+		$('.modal-alert .modal-header h4').text('Success!');
+		$('.modal-alert .modal-body p').html(msg);
+		$('.modal-alert').modal('show');
+		$('.modal-alert button').click(function(){window.location.href = '/';})
+		setTimeout(function(){window.location.href = '/';}, 3000);
+	}
 }
 
+//**PrintController.prototype.onUpdateSuccess = function()
+//**{
+//**	$('.modal-alert').modal({ show : false, keyboard : true, backdrop : true });
+//**	$('.modal-alert .modal-header h4').text('Success!');
+//**	$('.modal-alert .modal-body p').html('Your account has been updated.');
+//**	$('.modal-alert').modal('show');
+//**	$('.modal-alert button').off('click');
+//**}
