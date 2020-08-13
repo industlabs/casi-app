d97fa9c93797e1761bda9ad837a75505a7ddea69
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-13 14:26:52 -0500

    Create printController.js

---
+++ app/public/js/controllers/printController.js
@@ -0,0 +1,27 @@
+
+function PrintController()
+{
+// bind event listeners to button clicks //
+	var that = this;
+
+// handle user logout //
+	$('#btn-logout').click(function(){ that.attemptLogout(); });
+	}
+
+	this.attemptLogout = function()
+	{
+		var that = this;
+		$.ajax({
+			url: '/logout',
+			type: 'POST',
+			data: {logout : true},
+			success: function(data){
+	 			that.showLockedAlert('You are now logged out.<br>Redirecting you back to the homepage.');
+			},
+			error: function(jqXHR){
+				console.log(jqXHR.responseText+' :: '+jqXHR.statusText);
+			}
+		});
+	}
+}
+
