9f09b0f380084d5707a9a887cc90ec1d2d1a6471
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-13 23:59:17 -0500

    Update addcompany.js

--- app/server/views/addcompany.js
+++ app/server/views/addcompany.js
@@ -1,15 +1,3 @@
-//extends layout//
-
-//block content//
-
-        //include company//
-//	include modals/alert//
-
-//block scripts//
-	//script(src='/js/views/addcompany.js')//
-	//script(src='/js/controllers/addcompanyController.js')//
-	//script(src='/js/form-validators/accountValidator.js')//
-
 extends layout
 
 block content
