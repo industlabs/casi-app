475d89437afc3eed291fdc2daebe9acf4ff44b59
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-13 22:23:33 -0500

    Create company-signupController.js

---
+++ app/public/js/controllers/company-signupController.js
@@ -0,0 +1,8 @@
+function CompanysignupController()
+{
+// redirect to settings when cancel button is clicked //
+	$('#company-form-btn1').click(function(){ window.location.href = '/settings';});
+
+// redirect to homepage on new company creation, add short delay so user can read alert window //
+	$('.modal-alert #ok').click(function(){ setTimeout(function(){window.location.href = '/settings';}, 300)});
+}
