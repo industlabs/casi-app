cd4af89d636bc53b3b03cb99bc73d386831f1578
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-13 21:38:23 -0500

    Update account.js

--- app/public/js/views/account.js
+++ app/public/js/views/account.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
 	
-	var av = new AccountValidator();
+	//var av = new AccountValidator();
 	var sc = new SignupController();
 	
-	$('#account-form').ajaxForm({
-		beforeSubmit : function(formData, jqForm, options){
-			return av.validateForm();
-		},
-		success	: function(responseText, status, xhr, $form){
-			if (status == 'success') $('.modal-alert').modal('show');
-		},
-		error : function(e){
-			if (e.responseText == 'email-taken'){
-				av.showInvalidEmail();
-			}	else if (e.responseText == 'username-taken'){
-				av.showInvalidUserName();
-			}
-		}
-	});
-	$('#name-tf').focus();
+	//$('#company-form').ajaxForm({
+	//	beforeSubmit : function(formData, jqForm, options){
+	//		return av.validateForm();
+	//	},
+	//	success	: function(responseText, status, xhr, $form){
+	//		if (status == 'success') $('.modal-alert').modal('show');
+	//	},
+	//	error : function(e){
+	//		if (e.responseText == 'email-taken'){
+	//			av.showInvalidEmail();
+	//		}	else if (e.responseText == 'username-taken'){
+	//			av.showInvalidUserName();
+	//		}
+	//	}
+	//});
+	//$('#name-tf').focus();
 	
-// customize the account signup form //
+// customize the company signup form //
 	
-	$('#account-form h2').text('Signup');
-	$('#account-form #sub').text('Please tell us a little about yourself');
-	$('#account-form-btn1').html('Cancel');
-	$('#account-form-btn2').html('Submit');
-	$('#account-form-btn2').addClass('btn-primary');
+	$('#company-form h2').text('Signup');
+	$('#company-form #sub').text('Please tell us a little about company');
+	$('#company-form-btn1').html('Cancel');
+	$('#company-form-btn2').html('Submit');
+	$('#company-form-btn2').addClass('btn-primary');
 	
-// setup the alert that displays when an account is successfully created //
+// setup the alert that displays when an company is successfully created //
 
 	$('.modal-alert').modal({ show:false, keyboard : false, backdrop : 'static' });
-	$('.modal-alert .modal-header h4').text('Account Created!');
-	$('.modal-alert .modal-body p').html('Your account has been created.</br>Click OK to return to the login page.');
+	$('.modal-alert .modal-header h4').text('Company Created!');
+	$('.modal-alert .modal-body p').html('Your Company has been created.</br>Click OK to return to the login page.');
 
 });
