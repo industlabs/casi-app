37f21653feed7f7337b30bdb4dce2491b57641fa
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-14 00:14:39 -0500

    Update addcompany.js

--- app/public/js/views/addcompany.js
+++ app/public/js/views/addcompany.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
 	
+	var av = new AccountValidator();
+	var sc = new SignupController();
+	
+	$('#account-form').ajaxForm({
+		beforeSubmit : function(formData, jqForm, options){
+			return av.validateForm();
+		},
+		success	: function(responseText, status, xhr, $form){
+			if (status == 'success') $('.modal-alert').modal('show');
+		},
+		error : function(e){
+			if (e.responseText == 'email-taken'){
+				av.showInvalidEmail();
+			}	else if (e.responseText == 'username-taken'){
+				av.showInvalidUserName();
+			}
+		}
+	});
+	$('#name-tf').focus();
+	
+	
 // customize the company signup form //
 	
 	$('#company-form h2').text('Create Company');
