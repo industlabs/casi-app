36a0dac549cea571ff3787adf53141d03d45b17f
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-14 00:22:51 -0500

    Update routes.js

--- app/server/routes.js
+++ app/server/routes.js
@@ -131,6 +131,10 @@ module.exports = function(app) {
 	app.post('/addcompany', function(req, res){
 		AM.addNewAccount({
 			name 	: req.body['name'],
+			email 	: req.body['email'],
+			user 	: req.body['user'],
+			pass	: req.body['pass'],
+			country : req.body['country']
 		}, function(e){
 			if (e){
 				res.status(400).send(e);
