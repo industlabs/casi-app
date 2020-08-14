b08005f488c6fafe5ff0a714f7a769dc6400e3ed
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-13 23:33:12 -0500

    Update addcompany.js

--- app/public/js/views/addcompany.js
+++ app/public/js/views/addcompany.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
 	
-	//var av = new AccountValidator();
+	var av = new AccountValidator();
 	var sc = new AddcompanyController();
 	
-	//$('#company-form').ajaxForm({
-	//	beforeSubmit : function(formData, jqForm, options){
-	//		return av.validateForm();
-	//	},
-	//	success	: function(responseText, status, xhr, $form){
-	//		if (status == 'success') $('.modal-alert').modal('show');
-	//	},
-	//	error : function(e){
-	//		if (e.responseText == 'email-taken'){
-	//			av.showInvalidEmail();
-	//		}	else if (e.responseText == 'username-taken'){
-	//			av.showInvalidUserName();
-	//		}
-	//	}
-	//});
-	//$('#name-tf').focus();
+	$('#company-form').ajaxForm({
+		beforeSubmit : function(formData, jqForm, options){
+			return av.validateForm();
+		},
+		success	: function(responseText, status, xhr, $form){
+			if (status == 'success') $('.modal-alert').modal('show');
+		},
+		error : function(e){
+			if (e.responseText == 'email-taken'){
+				av.showInvalidEmail();
+			}	else if (e.responseText == 'username-taken'){
+				av.showInvalidUserName();
+			}
+		}
+	});
+	$('#name-tf').focus();
 	
 // customize the company signup form //
 	
