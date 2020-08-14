0f77aea7c58a7a62c19435b7a65ec4e53d4ff691
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-13 22:38:34 -0500

    Update and rename company.js to addcompany.js

--- app/public/js/views/addcompany.js
+++ app/public/js/views/addcompany.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 	
 // customize the company signup form //
 	
-	$('#company-form h2').text('Signup');
+	$('#company-form h2').text('Create Company');
 	$('#company-form #sub').text('Please tell us a little about company');
 	$('#company-form-btn1').html('Cancel');
 	$('#company-form-btn2').html('Submit');
@@ -32,6 +32,6 @@ $(document).ready(function(){
 
 	$('.modal-alert').modal({ show:false, keyboard : false, backdrop : 'static' });
 	$('.modal-alert .modal-header h4').text('Company Created!');
-	$('.modal-alert .modal-body p').html('Your Company has been created.</br>Click OK to return to the login page.');
+	$('.modal-alert .modal-body p').html('Your Company has been created.</br>Click OK to return to the Settings page.');
 
 });
