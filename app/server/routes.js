fb745bc9f80912efe4c20af32a189ad3fd435c47
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-14 00:24:54 -0500

    Update routes.js

--- app/server/routes.js
+++ app/server/routes.js
@@ -125,7 +125,7 @@ module.exports = function(app) {
 */
 
 	app.get('/addcompany', function(req, res) {
-		res.render('addcompany', {  title: 'Signup', countries : CT });
+		res.render('signup', {  title: 'Signup', countries : CT });
 	});
 	
 	app.post('/addcompany', function(req, res){
